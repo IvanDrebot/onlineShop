f94bd815acfa311737ddffbc918d4bfd3405f844
IvanDrebot <drebot99@gmail.com>
2019-05-07 14:35:34 +0300

    fix filters

--- src/app/product/product-grid/product-grid.component.ts
+++ src/app/product/product-grid/product-grid.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild, OnDestroy, Input} from '@angular/core';
+import {Component, OnInit, ViewChild, OnDestroy, Input, Renderer2} from '@angular/core';
 import {ProductService} from '../../../services/product.service';
 import {Product} from '../../../models/Product';
 import {ActivatedRoute} from '@angular/router';
@@ -29,11 +29,20 @@ export class ProductGridComponent implements OnInit, OnDestroy {
   constructor(
     private productService: ProductService,
     private filterService: FilterServiceService,
-    private router: ActivatedRoute
+    private router: ActivatedRoute,
+    private renderer: Renderer2
   ) { }
 
   ngOnInit() {
     this.getCategoryId();
+    const div = this.renderer.createElement('div');
+    this.renderer.addClass(div, 'test-div');
+    const newDiv = this.renderer.createElement('div');
+    this.renderer.appendChild(div, newDiv);
+    this.renderer.appendChild(document.body, div);
+    this.renderer.listen(div, 'click', (event) => {
+      console.log(event);
+    });
   }
 
   ngOnDestroy() {
@@ -48,6 +57,8 @@ export class ProductGridComponent implements OnInit, OnDestroy {
       this.loading = false;
       // @ts-ignore
       this.count = res.count;
+      console.log(this.count);
+
       this.filterService.subject.next(this.products[0]);
     });
   }
@@ -59,21 +70,24 @@ export class ProductGridComponent implements OnInit, OnDestroy {
     });
   }
 
-  loadMore() {
-    this.query.skip += 2;
-    this.loading = true;
-    this.getAllProduct();
-  }
-
   getCountOfPages(quntityProduct) {
     const countOfPages = quntityProduct / this.query.limit;
     for (let i = 1; i <= countOfPages; i++) {
       this.arrayOfPages.push(i);
     }
+    console.log(this.arrayOfPages);
   }
 
-  // changePage(page) {
-  //   this.query.skip = this.query.limit * page;
+  changePage(page) {
+    this.query.skip = this.query.limit * page;
+    this.getAllProduct();
+  }
+
+
+
+  // loadMore() {
+  //   this.query.skip += 2;
+  //   this.loading = true;
   //   this.getAllProduct();
   // }
 
